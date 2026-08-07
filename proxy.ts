8a313e3246b093fbecafdfa9fd1dcd1feaf14ea1
myapptly <tbledsoe1954@gmail.com>
2026-08-07 16:04:10 +0000

    Fix Next.js proxy export

--- proxy.ts
+++ proxy.ts
@@ -1,3 +1,3 @@
 export { createClient, supabase } from "./lib/supabase/client";
 export { createServerClient, getSupabaseClaims } from "./lib/supabase/server";
-export { createSupabaseProxy } from "./lib/supabase/proxy";
+export { createSupabaseProxy as proxy} from "./lib/supabase/proxy";
